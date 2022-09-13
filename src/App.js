9d74de2053833ac7758257e27170d88cf12f208b
Martin <martin.loncar@hivetech.hr>
2022-09-13 13:50:18 +0200

    added navbar, prepared additional skeleton components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
-import "./App.css";
+import "../src/css/App.css";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
