bd21572399041a47947985e101f7b4f6aed37b11
Martin <martin.loncar@hivetech.hr>
2022-09-15 11:28:01 +0200

    Moved one useState to Home component, made first steps for horizontall bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,9 +3,8 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { exercisesOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyparts] = useState([]);
 
   useEffect(() => {
@@ -37,8 +36,6 @@ const SearchExercises = () => {
       setSearch("");
       setExercises(searchedExercises);
     }
-
-    console.log(exercises);
   };
 
   return (
@@ -85,7 +82,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
